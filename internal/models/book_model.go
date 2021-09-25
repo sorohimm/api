f3bc7c53560f16108aeb26bee2ae080b3e6b35d8
Vladimir <sorohimm@gmail.com>
2021-09-25 23:11:44 +0300

    upd

--- internal/models/book_model.go
+++ internal/models/book_model.go
@@ -1,17 +1,16 @@
 package models
 
 type Book struct {
-	ID           string `json:"id"`
-	Name         string `json:"name"`
-	Year         string `json:"year"`
-	Author       string `json:"author"`
-	Category     string `json:"category"`
-	Price        string `json:"price"`
-	Descriptions string `json:"descriptions"`
+	Uuid         string `json:"name"         validate:"required"`
+	Year         string `json:"year"         validate:"required"`
+	Author       string `json:"author"       validate:"required"`
+	Category     string `json:"category"     validate:"required"`
+	Price        string `json:"price"        validate:"required"`
+	Descriptions string `json:"descriptions" validate:"required"`
 }
 
 type BookResponse struct {
-	ID           string `json:"id"`
+	Uuid         string `json:"uuid"`
 	Name         string `json:"name"`
 	Year         string `json:"year"`
 	Author       string `json:"author"`
