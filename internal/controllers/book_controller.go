30d0e7afb17c334a6a9f6e5984f61c4f6d385f06
Vladimir <sorohimm@gmail.com>
2021-09-25 23:08:40 +0300

    Error handling upd

--- internal/controllers/book_controller.go
+++ internal/controllers/book_controller.go
@@ -4,11 +4,12 @@ import (
 	"api/internal/interfaces"
 	"api/internal/models"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"go.uber.org/zap"
-	"net/http"
 )
 
 type BookControllers struct {
@@ -19,70 +20,84 @@ type BookControllers struct {
 func (c *BookControllers) CreateBook(ctx *gin.Context) {
 	var book models.Book
 
-	err := ctx.BindJSON(&book)
+	validate := validator.New()
+	err := validate.Struct(&book)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"Validate": "invalid request"})
+		return
+	}
+
+	err = ctx.BindJSON(&book)
 	if err != nil {
 		c.Log.Warn(err.Error())
 		ctx.JSON(http.StatusBadRequest, gin.H{
-			"BindJson": err.Error()})
-	}
-	validate := validator.New()
-	err = validate.Struct(&book)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"Validate": err.Error()})
+			"BindJson": "invalid json"})
+		return
 	}
 
-	//TODO: validate
-
-	id, err := c.BookService.CreateBook(book)
+	result, err := c.BookService.CreateBook(book)
 	if err != nil {
 		c.Log.Error(err.Error())
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"CreateBook": err.Error()})
+		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"id":   id,
-		"book": book,
-	})
+	ctx.JSON(http.StatusOK, result)
 }
 
 func (c *BookControllers) GetBook(ctx *gin.Context) {
 	id := ctx.Param("id")
 	_, err := uuid.Parse(id)
+
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, "GetBook")
+		c.Log.Error(err.Error())
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"Validate": err.Error()})
+		return
 	}
 
 	book, err := c.BookService.GetBook(id)
 	if err != nil {
 		c.Log.Error(err.Error())
-		ctx.JSON(http.StatusNotFound, "GetBook")
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"GetBook": err.Error()})
+		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"result": book,
-	})
+	ctx.JSON(http.StatusOK, book)
 }
 
 func (c *BookControllers) GetAllBooks(ctx *gin.Context) {
 	books, err := c.BookService.GetAllBooks()
 	if err != nil {
 		c.Log.Error(err.Error())
-		ctx.JSON(http.StatusInternalServerError, "GetAllBooks")
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"PullAllBooks": err.Error()})
+		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"result": books,
-	})
+	ctx.JSON(http.StatusOK, books)
 }
 
 func (c *BookControllers) DeleteBook(ctx *gin.Context) {
 	id := ctx.Param("id")
-	err := c.BookService.DeleteBook(id)
+	_, err := uuid.Parse(id)
+
 	if err != nil {
 		c.Log.Error(err.Error())
-		ctx.JSON(http.StatusInternalServerError, "DeleteBook")
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"Validate": err.Error()})
+		return
+	}
+
+	err = c.BookService.DeleteBook(id)
+	if err != nil {
+		c.Log.Error(err.Error())
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"DeleteBook": err.Error()})
+		return
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{
